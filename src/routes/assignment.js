479b4ac2e7ba106d5cb97cd9acb8c339c3217539
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-11-08 21:26:26 +0600

    get signle data route

--- src/routes/assignment.js
+++ src/routes/assignment.js
@@ -2,6 +2,8 @@ const express = require("express");
 const verifyToken = require("../middleware/verifyToken");
 const serverError = require("../utility/serverError");
 const { assignmentColl } = require("../db/mongoDB");
+const verifyTokenAndKey = require("../middleware/verifyTokenKey");
+const { ObjectId } = require("mongodb");
 
 const route = express.Router();
 
@@ -15,9 +17,22 @@ route.post("/new", verifyToken, (req, res) => {
   serverError(async () => {
     const result = await assignmentColl.insertOne(assignmentData);
     if (result.acknowledged) {
-      res.status(201).send({ success: true });
+      res.status(201).send({ success: true, itemId: result.insertedId });
     } else {
       res.status(400).send({ success: false });
+    }
+  }, res);
+});
+
+route.get("/:assignmentID", verifyToken, verifyTokenAndKey, (req, res) => {
+  const assignmentID = req.params.assignmentID;
+  serverError(async () => {
+    const query = { _id: new ObjectId(assignmentID) };
+    const result = await assignmentColl.findOne(query);
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(400).send({ message: false });
     }
   }, res);
 });
